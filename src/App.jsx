6923c7f98e0877fcc3fca55fe402a5972c562f60
Renata <renataleitesg@gmail.com>
2022-08-03 09:28:40 -0300

    create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hello World
-      </h1>
       <Header />
-      <Post author='Renata' content='lorem ipsum 123' />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author='Renata' content='lorem ipsum 123' />
+        </main>
+      </div>
     </>
   )
 }
