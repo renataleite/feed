c79af1a09ac6b8e60071b8c997aec88594facaf1
Renata <renataleitesg@gmail.com>
2022-08-04 15:20:28 -0300

    create Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src='https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40' />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/57663969?s=400&v=4' />
+                <Avatar src='https://avatars.githubusercontent.com/u/57663969?s=400&v=4' />
                 <strong>Renata Guedes</strong>
                 <span>Web Developer</span>
             </div>
