237bb7952d570a625e8466b3deff67833b649edc
Renata <renataleitesg@gmail.com>
2022-08-05 10:50:34 -0300

    add the properties to the Post component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 import './global.css'
 import { Sidebar } from "./components/Sidebar"
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/57663969?v=4',
+      name: 'Renata Leite',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraaa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'hrjane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraaa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'hrjane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  }
+]
+
 function App() {
 
   return (
@@ -13,12 +44,20 @@ function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>
-          <Post author='Renata' content='lorem ipsum 123' />
+        <main>{
+          post.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })
+        }
         </main>
       </div>
     </>
   )
 }
-
-export default App
+export default App;
