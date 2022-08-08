f4bea84034ffcbc9d5e133991854c4a7b1ca6994
Renata <renataleitesg@gmail.com>
2022-08-08 15:28:27 -0300

    implements adding likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { Trash, ThumbsUp } from 'phosphor-react'
+import { useState } from 'react';
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -26,10 +33,10 @@ export function Comment({ content, onDeleteComment }) {
                         {content}
                     </p>
                     <footer>
-                        <button>
+                        <button onClick={handleLikeComment}>
                             <ThumbsUp />
 
-                            Aplaudir <span>20</span></button>
+                            Aplaudir <span>{likeCount}</span></button>
                     </footer>
                 </div>
             </div>
